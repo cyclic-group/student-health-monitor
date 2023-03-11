3e009890f2adb0caccb216885dbcfb8ece2360a8
Sleipnir2000 <jionghua_chen@ucsb.edu>
2023-03-11 01:22:56 -0800

    Remove network constraint

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module StudentHealthMonitor
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.hosts << /.*\.ngrok\.io/
   end
 end
