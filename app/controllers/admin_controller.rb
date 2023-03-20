d843535528be690a5dfde5462d8b414dedeefcda
Sleipnir2000 <jionghua_chen@ucsb.edu>
2023-03-20 08:52:08 -0700

    create files

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,13 @@
+class AdminController < ApplicationController
+    # 主页
+    def show_home
+    end
+
+    # 发布公告页面
+    def new_announcement
+    end
+
+    # 处理提交的公告
+    def create_announcement
+    end
+end
