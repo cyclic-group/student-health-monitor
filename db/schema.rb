d874bf75ff857780fea25589a60fc8ba4980e754
Sleipnir2000 <jionghua_chen@ucsb.edu>
2023-03-20 10:09:20 -0700

    drop announcements

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_163811) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_20_170833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "announcements", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
